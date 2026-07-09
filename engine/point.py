383d20c2e1f44285c0a1e5d6a723d4f189bd7935
AmirhosseinKhazaee <khazaeeaslamirhossein@gmail.com>
2026-07-09 22:05:14 +0330

    add add_checker method to point

--- engine/point.py
+++ engine/point.py
@@ -2,19 +2,35 @@ from dataclasses import dataclass
 from engine.color import Color
 from engine.point_index import PointIndex
 
-@dataclass()
+
+@dataclass
 class Point:
     index: PointIndex
     color: Color | None
     checker_count: int
+
     def __post_init__(self):
-        if self.checker_count < 0 :
+        if self.checker_count < 0:
             raise ValueError("checker_count cannot be negative")
         if self.checker_count == 0 and self.color is not None:
             raise ValueError("color of point with zero checker count should be none !")
+
     def remove_checker(self):
-        if self.checker_count == 0 :
-            raise ValueError("this point already empty you can't remove checker from it !")
+        if self.checker_count == 0:
+            raise ValueError(
+                "this point already empty you can't remove checker from it !"
+            )
         self.checker_count -= 1
-        if self.checker_count == 0 :
+        if self.checker_count == 0:
             self.color = None
+
+    def add_checker(self, color : Color):
+        if self.checker_count == 0:
+            self.color = color
+            self.checker_count += 1
+        elif self.color != color:
+            raise ValueError(
+                f"you cannot add this color , the point is already filled with another color"
+            )
+        else:
+            self.checker_count += 1
